7c496cf6eed9d48ecec18e15e26ec9e1d35a623c
NotHydra <rizky.irswanda115@gmail.com>
2023-01-29 14:44:19 +0800

    feat: script component

---
+++ sources/components/script.php
@@ -0,0 +1,91 @@
+<script src="<?php echo $sourcePath ?>/public/plugins/jquery/jquery.min.js"></script>
+<script src="<?php echo $sourcePath ?>/public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo $sourcePath ?>/public/dist/js/adminlte.js"></script>
+
+<script>
+    function defaultColorTheme() {
+        const bodyElement = document.getElementById("body-theme");
+        const navElement = document.getElementById("nav-theme");
+        const asideElement = document.getElementById("aside-theme");
+        const iconElement = document.getElementById("icon-theme");
+        const textElement = document.getElementById("text-theme");
+
+        if (localStorage.colorThemeType == "light") {
+            bodyElement.classList.remove("dark-mode");
+            bodyElement.classList.add("light-mode");
+
+            navElement.classList.remove("navbar-dark");
+            navElement.classList.add("navbar-light");
+
+            asideElement.classList.remove("sidebar-dark-primary");
+            asideElement.classList.add("sidebar-light-primary");
+
+            iconElement.classList.remove("fa-moon");
+            iconElement.classList.add("fa-sun");
+
+            textElement.innerHTML = "Tema Terang";
+        } else if (localStorage.colorThemeType == "dark") {
+            bodyElement.classList.remove("light-mode");
+            bodyElement.classList.add("dark-mode");
+
+            navElement.classList.remove("navbar-light");
+            navElement.classList.add("navbar-dark");
+
+            asideElement.classList.remove("sidebar-light-primary");
+            asideElement.classList.add("sidebar-dark-primary");
+
+            iconElement.classList.remove("fa-sun");
+            iconElement.classList.add("fa-moon");
+
+            textElement.innerHTML = "Tema Gelap";
+        }
+    }
+
+    function changeColorTheme() {
+        const bodyElement = document.getElementById("body-theme");
+        const navElement = document.getElementById("nav-theme");
+        const asideElement = document.getElementById("aside-theme");
+        const iconElement = document.getElementById("icon-theme");
+        const textElement = document.getElementById("text-theme");
+
+        if (localStorage.colorThemeType == "light") {
+            bodyElement.classList.remove("light-mode");
+            bodyElement.classList.add("dark-mode");
+
+            navElement.classList.remove("navbar-light");
+            navElement.classList.add("navbar-dark");
+
+            asideElement.classList.remove("sidebar-light-primary");
+            asideElement.classList.add("sidebar-dark-primary");
+
+            iconElement.classList.remove("fa-sun");
+            iconElement.classList.add("fa-moon");
+
+            textElement.innerHTML = "Tema Gelap";
+
+            localStorage.colorThemeType = "dark";
+        } else if (localStorage.colorThemeType == "dark") {
+            bodyElement.classList.remove("dark-mode");
+            bodyElement.classList.add("light-mode");
+
+            navElement.classList.remove("navbar-dark");
+            navElement.classList.add("navbar-light");
+
+            asideElement.classList.remove("sidebar-dark-primary");
+            asideElement.classList.add("sidebar-light-primary");
+
+            iconElement.classList.remove("fa-moon");
+            iconElement.classList.add("fa-sun");
+
+            textElement.innerHTML = "Tema Terang";
+
+            localStorage.colorThemeType = "light";
+        }
+    }
+
+    if (!localStorage.colorThemeType) {
+        localStorage.colorThemeType = "light";
+    };
+
+    defaultColorTheme()
+</script>
