b1c3c17eb73f1b9fc77effb0b6b61ff758d79aba
NotHydra <rizky.irswanda115@gmail.com>
2023-01-29 14:43:44 +0800

    feat: head component

---
+++ sources/components/head.php
@@ -0,0 +1,8 @@
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <title>Pelaporan | <?php echo $headTitle; ?></title>
+
+ <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+ <link rel="stylesheet" href="<?php echo $sourcePath; ?>/public/plugins/fontawesome-free/css/all.min.css">
+ <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+ <link rel="stylesheet" href="<?php echo $sourcePath; ?>/public/dist/css/adminlte.min.css">
