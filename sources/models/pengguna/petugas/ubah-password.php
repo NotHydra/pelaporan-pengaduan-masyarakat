af5b2ef5411490b333041e6facbe92ba0e65e41b
NotHydra <rizky.irswanda115@gmail.com>
2023-01-30 00:00:28 +0800

    feat: petugas password

---
+++ sources/models/pengguna/petugas/ubah-password.php
@@ -0,0 +1,121 @@
+<?php
+$sourcePath = "../../..";
+include "$sourcePath/utilities/environment.php";
+include "$sourcePath/utilities/connection.php";
+include "$sourcePath/utilities/session/start.php";
+
+include "$sourcePath/middlewares/isNotAuthenticated.php";
+
+include "$sourcePath/utilities/session/data.php";
+include "$sourcePath/utilities/date.php";
+
+$id = $_GET["id"];
+if (mysqli_num_rows(mysqli_query($connection, "SELECT id FROM petugas WHERE id='$id' and dihapus='0';")) <= 0) {
+  echo "<script>window.location='.';</script>";
+};
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php
+  $headTitle = "Petugas";
+  include "$sourcePath/components/head.php";
+  include "$sourcePath/utilities/modal.php";
+  ?>
+</head>
+
+<body class="hold-transition layout-navbar-fixed layout-fixed light-mode" id="body-theme">
+  <div class="wrapper">
+    <?php
+    $navActive = [2, 1];
+    include "$sourcePath/components/nav.php";
+    ?>
+
+    <div class="content-wrapper">
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-sm">
+              <div class="card">
+                <?php
+                $pageItemObject = $pageArray[$navActive[0]]["child"][$navActive[1]];
+                $extraTitle = "Ubah Password";
+                include "$sourcePath/components/content/head.php";
+                ?>
+
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col-sm">
+                      <form action="<?php $_SERVER["PHP_SELF"]; ?>?id=<?php echo $id; ?>" method="POST" onsubmit="return confirmModal('form', this);">
+                        <?php
+                        $inputArray = array(
+                          array(
+                            "id" => 1,
+                            "display" => "Password",
+                            "name" => "password",
+                            "type" => "password",
+                            "value" => isset($_POST["password"]) ? $_POST["password"] : null,
+                            "placeholder" => "Masukkan password disini",
+                            "enable" => true
+                          ),
+                          array(
+                            "id" => 2,
+                            "display" => "Konfirmasi Password",
+                            "name" => "konfirmasi_password",
+                            "type" => "password",
+                            "value" => isset($_POST["konfirmasi_password"]) ? $_POST["konfirmasi_password"] : null,
+                            "placeholder" => "Masukkan konfirmasi password disini",
+                            "enable" => true
+                          )
+                        );
+
+                        include "$sourcePath/components/input/detail.php";
+                        ?>
+
+                        <button class="btn btn-danger btn-block" type="submit"><i class="fa fa-lock"></i> Ubah Password</button>
+                        <a class="btn btn-danger btn-block" role="button" onclick="confirmModal('location', '.');"><i class="fa fa-undo"></i> Kembali</a>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <?php
+    include "$sourcePath/components/footer.php";
+    ?>
+  </div>
+
+  <?php
+  include "$sourcePath/components/script.php";
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $password = md5($_POST["password"]);
+    $konfirmasiPassword = md5($_POST["konfirmasi_password"]);
+
+    if ($password == $konfirmasiPassword) {
+      try {
+        $result = mysqli_query($connection, "UPDATE petugas SET password='$password' WHERE id='$id';");
+
+        if ($result) {
+          echo "<script>successModal(null, null);</script>";
+        } else {
+          echo "<script>errorModal(null, null);</script>";
+        };
+      } catch (exception $e) {
+        echo "<script>errorModal(null, null);</script>";
+      };
+    } else {
+      echo "<script>errorModal('Konfirmasi password salah', null);</script>";
+    };
+  };
+  ?>
+</body>
+
+</html>
