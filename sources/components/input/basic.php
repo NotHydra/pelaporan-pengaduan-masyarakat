ed94d67d02d4374df4af4e9cd08dbebccdd3dc4d
NotHydra <rizky.irswanda115@gmail.com>
2023-01-29 18:33:22 +0800

    feat: input basic component

---
+++ sources/components/input/basic.php
@@ -0,0 +1,14 @@
+<?php
+foreach ($inputArray as $inputObject) {
+?>
+    <div class="input-group mb-3">
+        <input type="<?php echo $inputObject["type"]; ?>" class="form-control" id="<?php echo $inputObject["name"]; ?>" name="<?php echo $inputObject["name"]; ?>" value="<?php echo $inputObject["value"]; ?>" placeholder="<?php echo $inputObject["display"]; ?>" required>
+        <div class="input-group-append">
+            <div class="input-group-text">
+                <span class="fas fa-<?php echo $inputObject["icon"]; ?>"></span>
+            </div>
+        </div>
+    </div>
+<?php
+}
+?>
