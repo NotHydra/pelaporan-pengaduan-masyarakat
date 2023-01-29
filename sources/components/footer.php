5f56be208d9c43558ffc84d5ebec2fbb01d8566f
NotHydra <rizky.irswanda115@gmail.com>
2023-01-29 14:44:07 +0800

    feat: footer component

---
+++ sources/components/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <b>Copyright &copy; <?php echo currentYear() ?> XII RPL</b>
+
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Version Beta</b>
+    </div>
+</footer>
