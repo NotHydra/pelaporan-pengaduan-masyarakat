b22a4509d1ba5357e1635e0e4c7943d3d4eedeb2
NotHydra <rizky.irswanda115@gmail.com>
2023-01-31 23:12:56 +0800

    feat: tanggapan list

---
+++ sources/models/tanggapan/daftar.php
@@ -0,0 +1,220 @@
+<?php
+$sourcePath = "../..";
+include "$sourcePath/utilities/environment.php";
+include "$sourcePath/utilities/connection.php";
+include "$sourcePath/utilities/session/start.php";
+
+include "$sourcePath/middlewares/isNotAuthenticated.php";
+
+include "$sourcePath/utilities/session/data.php";
+include "$sourcePath/utilities/role.php";
+include "$sourcePath/utilities/date.php";
+
+roleGuardMinimum($sessionLevel, "petugas", "/$originalPath");
+
+$idPengaduan = $_GET["id"];
+$resultPengaduan = mysqli_query($connection, "SELECT status FROM pengaduan WHERE id='$idPengaduan' AND (status='diproses' OR status='selesai') AND dihapus='0';");
+$dataPengaduan = mysqli_fetch_assoc($resultPengaduan);
+if (mysqli_num_rows($resultPengaduan) <= 0) {
+  echo "<script>window.location='.';</script>";
+};
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php
+  $headTitle = "Tanggapan";
+  include "$sourcePath/components/head.php";
+  include "$sourcePath/components/data-table/head.php";
+  include "$sourcePath/components/select/head.php";
+  include "$sourcePath/utilities/modal.php";
+  ?>
+</head>
+
+<body class="hold-transition layout-navbar-fixed layout-fixed light-mode" id="body-theme">
+  <div class="wrapper">
+    <?php
+    $navActive = [5, null];
+    include "$sourcePath/components/nav.php";
+    ?>
+
+    <div class="content-wrapper">
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-sm">
+              <div class="card">
+                <?php
+                $pageItemObject = $pageArray[$navActive[0]];
+                $extraTitle = "Daftar";
+                include "$sourcePath/components/content/head.php";
+                ?>
+
+                <div class="card-body">
+                  <form class="row mb-2" action="<?php $_SERVER['PHP_SELF']; ?>" method="POST">
+                    <div class="col-sm">
+                      <?php
+                      $inputArray = [
+                        [
+                          "id" => 1,
+                          "display" => null,
+                          "name" => "tahun",
+                          "type" => "select",
+                          "value" => [
+                            array_merge([[0, "Semua"]], array_map(function ($yearObject) {
+                              return [$yearObject[0], $yearObject[0]];
+                            }, mysqli_fetch_all(mysqli_query($connection, "SELECT DISTINCT YEAR(dibuat) FROM tanggapan WHERE id_pengaduan='$idPengaduan' AND dihapus='0' ORDER BY dibuat DESC;")))), isset($_POST["tahun"]) ? $_POST["tahun"] : null
+                          ],
+                          "placeholder" => "Pilih tahun disini",
+                          "enable" => true
+                        ],
+                      ];
+
+                      include "$sourcePath/components/input/detail.php";
+                      ?>
+                    </div>
+
+                    <div class="col-sm">
+                      <?php
+                      $inputArray = [
+                        [
+                          "id" => 1,
+                          "display" => null,
+                          "name" => "bulan",
+                          "type" => "select",
+                          "value" => [
+                            [
+                              [0, "Semua"],
+                              [1, "Januari"],
+                              [2, "Februari"],
+                              [3, "Maret"],
+                              [4, "April"],
+                              [5, "Mei"],
+                              [6, "Juni"],
+                              [7, "Juli"],
+                              [8, "Agustus"],
+                              [9, "September"],
+                              [10, "Oktober"],
+                              [11, "November"],
+                              [12, "Desember"]
+                            ], isset($_POST["bulan"]) ? $_POST["bulan"] : null
+                          ],
+                          "placeholder" => "Pilih bulan disini",
+                          "enable" => true
+                        ],
+                      ];
+
+                      include "$sourcePath/components/input/detail.php";
+                      ?>
+                    </div>
+
+                    <div class="col-sm">
+                      <button class="btn btn-primary btn-block" type="submit"><i class="fa fa-search"></i> Cari</button>
+                    </div>
+                  </form>
+
+                  <div class="row">
+                    <div class="col-sm">
+                      <table id="main-table" class="table table-bordered table-striped table-sm">
+                        <thead>
+                          <tr>
+                            <th class="text-center align-middle export">No.</th>
+                            <th class="text-center align-middle export">Petugas</th>
+                            <th class="text-center align-middle export">Isi Tanggapan</th>
+                            <th class="text-center align-middle export">Dibuat</th>
+                            <th class="text-center align-middle export">Diubah</th>
+
+                            <?php
+                            if ($dataPengaduan["status"] == "diproses") {
+                            ?>
+                              <th class="text-center align-middle export">Aksi</th>
+                            <?php
+                            }
+                            ?>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <?php
+                          $currentDate = date("Y-m-d H:i:s");
+
+                          $extraFilter = "";
+                          if (isset($_POST["tahun"])) {
+                            $tahunFilter = $_POST["tahun"];
+                            if ($tahunFilter != 0) {
+                              $extraFilter = $extraFilter . " AND YEAR(dibuat)='$tahunFilter'";
+                            };
+                          };
+
+                          if (isset($_POST["bulan"])) {
+                            $bulanFilter = $_POST["bulan"];
+                            if ($bulanFilter != 0) {
+                              $extraFilter = $extraFilter . " AND MONTH(dibuat)='$bulanFilter'";
+                            };
+                          };
+
+                          $result = mysqli_query($connection, "SELECT id, id_petugas, isi_tanggapan, dibuat, diubah FROM tanggapan WHERE id_pengaduan='$idPengaduan' AND dihapus='0' $extraFilter ORDER BY dibuat DESC;");
+                          foreach ($result as $i => $data) {
+                            $id = $data["id"];
+                            $idPetugas = $data["id_petugas"];
+                            $dataPetugas = mysqli_fetch_assoc(mysqli_query($connection, "SELECT nama FROM petugas WHERE id='$idPetugas' AND dihapus='0';"));
+                          ?>
+                            <tr>
+                              <td class="text-center align-middle"><?php echo $i + 1; ?>.</td>
+                              <td class="text-center align-middle"><?php echo $dataPetugas["nama"]; ?></td>
+                              <td class="align-middle"><?php echo $data["isi_tanggapan"]; ?></td>
+                              <td class="text-center align-middle"><?php echo $data["dibuat"]; ?></td>
+                              <td class="text-center align-middle"><?php echo dateInterval($data["diubah"], $currentDate); ?></td>
+
+                              <?php
+                              if ($dataPengaduan["status"] == "diproses") {
+                              ?>
+                                <td class="text-center align-middle">
+                                  <div class="btn-group">
+                                    <a class="btn btn-app bg-danger m-0" href="./hapus.php?id=<?php echo $id; ?>&idPengaduan=<?php echo $idPengaduan; ?>">
+                                      <i class=" fas fa-trash"></i> Hapus
+                                    </a>
+                                  </div>
+                                </td>
+                              <?php
+                              }
+                              ?>
+                            </tr>
+                          <?php
+                          };
+                          ?>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+
+                  <?php
+                  if ($dataPengaduan["status"] == "diproses") {
+                  ?>
+                    <a class="btn btn-primary btn-block mt-1" href="./buat.php?id=<?php echo $idPengaduan; ?>"><i class="fa fa-plus"></i> Buat</a>
+                  <?php
+                  }
+                  ?>
+                  <a class="btn btn-danger btn-block" role="button" onclick="confirmModal('location', '.');"><i class="fa fa-undo"></i> Kembali</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <?php
+    include "$sourcePath/components/footer.php";
+    ?>
+  </div>
+
+  <?php
+  include "$sourcePath/components/script.php";
+  include "$sourcePath/components/data-table/script.php";
+  include "$sourcePath/components/select/script.php";
+  ?>
+</body>
+
+</html>
